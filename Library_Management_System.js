29a89da01224188e3770feb77fad6894c364bbec
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-20 16:59:48 -0400

    Create Book class with properties and methods

--- Library_Management_System.js
+++ Library_Management_System.js
@@ -1,1 +1,20 @@
+class Book {
+    constructor(title,author,ISBN){
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this._isAvailable = true;
+    }
 
+    getDetails(){
+        console.log(`Title: ${this.title}, Author: ${this.author},ISBN: ${this.ISBN}`);
+    }
+
+    get isAvailable(){
+        console.log(this._isAvailable);
+    }
+
+    set isAvailable(availability){
+    this._isAvailable = availability;
+    }
+}
